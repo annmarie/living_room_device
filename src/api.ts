20df9036ed71bdb85ca37db467b8051fa06860da
Annmarie Conway <annmarie.daylife@gmail.com>
2025-03-14 19:51:09 -0400

    fixed api caching

--- src/api.ts
+++ src/api.ts
@@ -2,40 +2,30 @@ import { HubData, Item } from './interfaces';
 
 const CACHE_DURATION = 5 * 60 * 1000;
 
-export async function fetchHubData(): Promise<HubData> {
-  const cacheKey = 'hubData' as string;
-  const cachedData = localStorage.getItem(cacheKey) as string;
-  const cachedTimestamp = localStorage.getItem(`${cacheKey}_timestamp`) as string;
+async function fetchData<T>(url: string, cacheKey: string, key?: string): Promise<T> {
+  const cachedData = localStorage.getItem(cacheKey);
+  const cachedTimestamp = localStorage.getItem(`${cacheKey}_timestamp`);
 
   if (cachedData && cachedTimestamp) {
-    const age = Date.now() - parseInt(cachedTimestamp, 10) as number;
+    const age = Date.now() - parseInt(cachedTimestamp, 10);
     if (age < CACHE_DURATION) {
       return JSON.parse(cachedData);
     }
   }
 
-  const response = await fetch(import.meta.env.VITE_HUB_URL);
+  const response = await fetch(url);
   const data = await response.json();
   localStorage.setItem(cacheKey, JSON.stringify(data));
   localStorage.setItem(`${cacheKey}_timestamp`, Date.now().toString());
-  return data;
+  return (key) ? data[key] : data
+}
+
+export async function fetchHubData(): Promise<HubData> {
+  const url = import.meta.env.VITE_HUB_URL;
+  return fetchData<HubData>(url, 'hubData');
 }
 
 export async function fetchCollectionData(id: string): Promise<Item[]> {
-  const cacheKey = `collectionData_${id}` as string;
-  const cachedData = localStorage.getItem(cacheKey) as string;
-  const cachedTimestamp = localStorage.getItem(`${cacheKey}_timestamp`) as string;
-
-  if (cachedData && cachedTimestamp) {
-    const age = Date.now() - parseInt(cachedTimestamp, 10) as number;
-    if (age < CACHE_DURATION) {
-      return JSON.parse(cachedData);
-    }
-  }
-
-  const response = await fetch(`${import.meta.env.VITE_ITEM_URL}/${id}.json`);
-  const data = await response.json() as { items: Item[] };
-  localStorage.setItem(cacheKey, JSON.stringify(data.items));
-  localStorage.setItem(`${cacheKey}_timestamp`, Date.now().toString());
-  return data.items;
+  const url = `${import.meta.env.VITE_ITEM_URL}/${id}.json`;
+  return fetchData<Item[]>(url, `collectionData_${id}`, 'items');
 }
